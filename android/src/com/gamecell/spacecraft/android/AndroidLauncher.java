cf0e164859bdf05bc025bb2a447c081e803ad00b
Firedark <firedarknes@gmail.com>
2015-10-12 17:53:18 +0200

    Initial commit

---
+++ android/src/com/gamecell/spacecraft/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.gamecell.spacecraft.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.gamecell.spacecraft.SpaceCraft;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new SpaceCraft(), config);
+	}
+}
