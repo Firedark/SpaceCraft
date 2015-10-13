2552509188180b53ee36674156ccc3ec90dff5f1
Firedark <firedarknes@gmail.com>
2015-10-13 18:35:39 +0200

    Prueba

--- desktop/src/com/gamecell/spacecraft/desktop/DesktopLauncher.java
+++ desktop/src/com/gamecell/spacecraft/desktop/DesktopLauncher.java
@@ -7,6 +7,8 @@ import com.gamecell.spacecraft.SpaceCraft;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.height = 1080;
+		config.width = 720;
 		new LwjglApplication(new SpaceCraft(), config);
 	}
 }
