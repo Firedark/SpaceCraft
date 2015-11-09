1ee000235295ce52a599ed756678c7ab98df56f9
jagimenose <jaume.gimeno@gmail.com>
2015-11-09 23:39:33 +0100

    Botones de menu de inicio

--- core/src/com/gamecell/spacecraft/SpaceCraft.java
+++ core/src/com/gamecell/spacecraft/SpaceCraft.java
@@ -1,16 +1,10 @@
 package com.gamecell.spacecraft;
 
 import com.badlogic.gdx.Game;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.gamecell.spacecraft.Screens.GameScreen;
 import com.gamecell.spacecraft.Screens.OptionsScreen;
 import com.gamecell.spacecraft.Screens.SplashScreen;
 import com.gamecell.spacecraft.Screens.StartScreen;
-
-import sun.management.snmp.jvminstr.JvmThreadInstanceEntryImpl;
 
 // Modificación 22/10/1
 public class SpaceCraft extends Game {
@@ -50,9 +44,9 @@ public class SpaceCraft extends Game {
 		//Carga las imagenes en memoria.
 
 
-
+			this.setScreen(startScreen);
 			//Carga la pantalla GameScreen.
-			this.setScreen(splashScreen);
+			//this.setScreen(splashScreen);
 
 
 	}
